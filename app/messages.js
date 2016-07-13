9ae5df112c1289e01111523d9331e8bf51701b01
alun <alun@ickery.com>
2016-07-13 16:23:11 +0100

    latest messages update
    Signed-off-by: alun <alun@ickery.com>

--- app/messages.js
+++ app/messages.js
@@ -4,14 +4,38 @@
 
 module.exports = {
   welcome_title: 'Welcome',
-  nameanddate_title: 'Name and Date',
-  address_title: 'Address',
+  nameanddate_title: '_nameanddate',
+  nameanddate_header: 'About the person who died',
+  nameanddate_question_1:'What was their full name?',
+  nameanddate_hint_1: 'As shown on their death certificate.',
+  nameanddate_stop_1: 'You need the death certificate to complete this application.',
+  nameanddate_question_2: 'Was the person who died known by another name?',
+  nameanddate_hint_2: 'A nickname, maiden name or title.',
+  nameanddate_question_3: 'What is the date they were born?',
+  nameanddate_hint_3: 'As shown on their birth certificate.',
+  nameanddate_question_4: 'Was is the date they died?',
+  nameanddate_hint_4: 'As shown on their death certificate.',
+  nameanddate_stop_4: 'You need the death certificate to complete this application.',
+
+  address_header: 'About the person who died',
+  address_question_1: 'Were they domiciled in England or Wale at the date of their death?',
+  address_hint_1: 'Where they considered to be their permanent home to be.',
+  address_question_2: 'What was their address?',
+  address_hint_2: 'Their permanent address at the time of their death.',
+
   maritalstatus_title: 'Marital Status',
+  maritalstatus_header: 'About the person who died',
+  maritalstatus_question_1: 'What was their Marital Status?',
+  maritalstatus_hint_1: 'Their martial status at the time of their death.',
+  maritalstatus_radio_1: 'Married',
+  maritalstatus_radio_2: 'Never married',
+  maritalstatus_radio_hint_2: 'They were a bachelor or spinster.',
   maritalstatus_radio_3: 'Widowed',
   maritalstatus_radio_hint_3: 'Their spouse died.', 
   maritalstatus_radio_4: 'Divorced, or their civil partnership was dissolved',
   maritalstatus_radio_5: 'Judicially separated',
   maritalstatus_radio_hint_5: 'They were legally separated.', 
+
   will_title: 'Will and Codicil',
   will_header: 'About the will',
   will_question_1: 'Did the person who died leave a will?',
@@ -24,12 +48,17 @@ module.exports = {
   will_question_hint_4: 'Will made in another country apart from England and Wales.',
   will_question_5: 'Are there any codicils?',
   will_question_hint_5: 'Changes or updates to the will.',
+  
   executors_title: 'The Executors',
   summary_title: 'Summary',
   nextsteps_title: 'Next Steps',
 
   stop_foreign_domicile: 'Deceased was not domiciled in England or Wales. The application would be stopped at this point.',
   stop_no_will: 'No will is available. The application would be stopped at this point.',
+  address_title: 'Address',
+
+
+
 
 
 };	
