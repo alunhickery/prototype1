2795c3e1bd259d1be4e9aafa7d58cb729db035cd
alun <alun@ickery.com>
2016-07-13 14:41:14 +0100

    updated with new content messages from fran
    Signed-off-by: alun <alun@ickery.com>

--- app/messages.js
+++ app/messages.js
@@ -3,15 +3,33 @@
 // Note: prototype config can be overridden using environment variables (eg on heroku)
 
 module.exports = {
-  welcome_title: "Welcome",
-  nameanddate_title: "Name and Date",
+  welcome_title: 'Welcome',
+  nameanddate_title: 'Name and Date',
   address_title: 'Address',
   maritalstatus_title: 'Marital Status',
+  maritalstatus_radio_3: 'Widowed',
+  maritalstatus_radio_hint_3: 'Their spouse died.', 
+  maritalstatus_radio_4: 'Divorced, or their civil partnership was dissolved',
+  maritalstatus_radio_5: 'Judicially separated',
+  maritalstatus_radio_hint_5: 'They were legally separated.', 
   will_title: 'Will and Codicil',
+  will_header: 'About the will',
+  will_question_1: 'Did the person who died leave a will?',
+  will_question_stop_1: 'Stop content: You will need the original will to complete this application.',
+  will_question_2: 'Do you have the original will?',
+  will_question_stop_2: 'Stop content: You will need the original will to complete this application.',
+  will_question_3: 'What is the date of the will?',
+  will_question_hint_3: 'The date the will was made.',
+  will_question_4: 'Did the person who died leave any wills outside of England and Wales?',
+  will_question_hint_4: 'Will made in another country apart from England and Wales.',
+  will_question_5: 'Are there any codicils?',
+  will_question_hint_5: 'Changes or updates to the will.',
   executors_title: 'The Executors',
   summary_title: 'Summary',
   nextsteps_title: 'Next Steps',
+
   stop_foreign_domicile: 'Deceased was not domiciled in England or Wales. The application would be stopped at this point.',
-  stop_no_will: 'No will is available. The application would be stopped at this point.'
-};
-	
+  stop_no_will: 'No will is available. The application would be stopped at this point.',
+
+
+};	
